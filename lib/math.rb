9b0c4f7896e9a7cbe03feb8d75a50157a4d6981c
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-15 20:24:53 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+sum = num1 + num2
+sum
 end
 
 def subtraction(num1, num2)
-
+result = num1 - num2
+result
 end
 
 def division(num1, num2)
-
+quotient = num1 / num2
+quotient
 end
 
 def multiplication(num1, num2)
-
+product = num1 * num2
+product
 end
 
 def modulo(num1, num2)
-
+remainder = num1 % num2
+remainder
 end
 
 def square_root(num)
-
+squared = num1**2
 end
